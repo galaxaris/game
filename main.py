f40b6a1c541124507a0bf175eade9fa55f842215
Axelotl_30 <axelgirard7@laposte.net>
2026-02-09 23:07:24 +0100

    tweaks of how width/height and x/y are managed, to fit the api

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from api.assets.Animation import Animation
 from api.assets.Resource import Resource, ResourceType
 from api.assets.Texture import Texture
 
-WIDTH, HEIGHT = 640, 360
-WIN_WIDTH, WIN_HEIGHT = 1600, 900
+RENDER_WIDTH, RENDER_HEIGHT = 640, 360
+WIDTH, HEIGHT = 1920, 1080
 NAME = "Omicronde"
 FPS = 60
 
-game = Game(WIN_WIDTH, WIN_HEIGHT, WIDTH, HEIGHT, NAME, pg.SCALED | pg.RESIZABLE, FPS)
+game = Game((WIDTH, HEIGHT), NAME, pg.RESIZABLE, FPS, (RENDER_WIDTH, RENDER_HEIGHT))
 
 game.set_icon(join("assets", "icon.jpg"))
 
@@ -21,14 +21,14 @@ glob = Resource(ResourceType.GLOBAL, "assets")
 anim = Animation(Texture("run.png", glob), 12, 100)
 icon = Texture("icon.jpg", glob)
 
-first = GameObject(0, 0, 100, 100)
-second = GameObject(0, 0, 100, 100)
-third = GameObject(0, 150, 300, 10)
+first = GameObject((0, 0), (100, 100))
+second = GameObject((0, 0), (100, 100))
+third = GameObject((0, 150), (300, 10))
 
-player = GameObject(0, 0, 100, 100)
-background = GameObject(0, 0, WIDTH, HEIGHT)
+player = GameObject((0, 0), (100, 100))
+background = GameObject((0, 0), (WIDTH, HEIGHT))
 
-aaa = GameObject(0, 0, 100, 100)
+aaa = GameObject((0, 0), (100, 100))
 aaa.set_color((0, 255, 0))
 
 def loop(self):
@@ -38,11 +38,10 @@ def loop(self):
 
     third.set_color((255,0,0))
 
-
     first.set_texture(icon)
     second.set_texture(icon)
-    first.set_position(100,100)
-    second.set_position((pg.time.get_ticks() - 600)%640, 1/1000*second.x**2)
+    first.set_position((100,100))
+    second.set_position(((pg.time.get_ticks() - 600)%640, int(1/1000*second.pos.x**2)))
 
     screen = self.screen
     screen.set_layer(1, "a")
@@ -57,28 +56,20 @@ def loop(self):
 
     keys_pressed = pg.key.get_pressed()
 
-
     #FOR AXEL
-
     screen.set_layer(3, "render")
-
-
-
     screen.default_surface.blit(aaa.image, aaa.rect)
 
-
-
     if keys_pressed[pg.K_d]:
-       player.set_position(player.x + 5, player.y)
+       player.set_position(player.pos + (5,0))
        player.set_direction("right")
     if keys_pressed[pg.K_q]:
-       player.set_position(player.x - 5, player.y)
+       player.set_position(player.pos + (-5, 0))
        player.set_direction("left")
     if keys_pressed[pg.K_s]:
-       player.set_position(player.x, player.y + 5)
+       player.set_position(player.pos + (0, 5))
     if keys_pressed[pg.K_z]:
-       player.set_position(player.x, player.y - 5)
-
+       player.set_position(player.pos + (0, -5))
 
 def main():
     game.run(loop)
