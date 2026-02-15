747a88682bd6b35aa03af639e222aff737b0d578
Raphix <picot.raphael@gmail.com>
2026-02-15 18:42:06 +0100

    feat: add gravity and Debug Tools

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # To run the game from CMD or VS Code terminal (PyCharm runs strangely)
 # Execute the program with "python -m game.main" from the root directory of the project
-
+from math import floor
 from os.path import join
 
 from pygame.surface import Surface
@@ -8,12 +8,15 @@ from pygame.surface import Surface
 from api.Game import Game
 import pygame as pg
 
-from api.GameObject import GameObject
+from api.UI.Text import Text
 from api.assets.Animation import Animation
 from api.assets.Resource import Resource, ResourceType
 from api.assets.Texture import Texture
+from api.entity.Player import Player
 from api.environment.Background import Background
 from api.environment.Parallax import ParallaxImage, ParallaxBackground
+from api.environment.Solid import Solid
+from api.utils import Debug
 
 RENDER_WIDTH, RENDER_HEIGHT = 640, 360
 WIDTH, HEIGHT = 1600, 900
@@ -24,68 +27,80 @@ game = Game((WIDTH, HEIGHT), (RENDER_WIDTH, RENDER_HEIGHT), NAME, pg.RESIZABLE |
 
 game.set_icon(join("assets", "icon.jpg"))
 
+#DEBUG MODE
+
+game.enable_debug()
+
 glob = Resource(ResourceType.GLOBAL, "assets")
 anim = Animation(Texture("run.png", glob), 12, 100)
+
 icon = Texture("icon.jpg", glob)
 blue_tile = Texture("Blue.png", glob)
+
 t_p1 = Texture("0.9x parallax-demon-woods-close-trees.png", glob)
 t_p2 = Texture("0.70x parallax-demon-woods-mid-trees.png", glob)
 t_p3 = Texture("0.5x parallax-demon-woods-far-trees.png", glob)
 t_p4 = Texture("0.25x parallax-demon-woods-bg.png", glob)
 
-player = GameObject((0, 0), (50, 50))
-player.set_position((310,170))
+player = Player((-10, 0), (50, 50))
+player.set_gravity(0.5)
+player.set_position((310,410))
 
-block = GameObject((0, 0), (100, 100))
-block.set_color((255, 255, 0))
-block.set_position((310,270))
+collections = [Solid((x,500), (100, 100)) for x in range(0, 500, 100)]
+collections += [Solid((100,y), (100, 100)) for y in range(200, 700, 100)]
+
+for coll in collections:
+    coll.set_color((200, 200, 200))
 
 game.screen.camera.set_offset((RENDER_WIDTH//2 - player.size.x,RENDER_HEIGHT//2 - player.size.y))
+game.screen.camera.set_limits((-RENDER_WIDTH, -RENDER_HEIGHT), (RENDER_WIDTH, RENDER_HEIGHT))
 
-aaa = GameObject((0, 0), (100, 100))
-aaa.set_color((255, 255, 255))
-
-p_bg = ParallaxBackground((RENDER_WIDTH, RENDER_HEIGHT))
-p1 = ParallaxImage(pg.Vector2(0.9, 0.45), t_p1)
-p2 = ParallaxImage(pg.Vector2(0.7, 0.35), t_p2)
-p3 = ParallaxImage(pg.Vector2(0.5, 0.25), t_p3)
-p4 = ParallaxImage(pg.Vector2(0.25, 0.125), t_p4)
+p_bg = ParallaxBackground((RENDER_WIDTH, RENDER_HEIGHT), [
+    ParallaxImage(pg.Vector2(0.9, 0.45), t_p1),
+    ParallaxImage(pg.Vector2(0.7, 0.35), t_p2),
+    ParallaxImage(pg.Vector2(0.5, 0.25), t_p3),
+    ParallaxImage(pg.Vector2(0.25, 0.125), t_p4)
+])
 
 bg = Background(blue_tile,True,(RENDER_WIDTH, RENDER_HEIGHT))
+screen = game.screen
 
-p_bg.add(p1)
-p_bg.add(p2)
-p_bg.add(p3)
-p_bg.add(p4)
+
+def debug_info():
+    game.debug(f"FPS : {int(game.clock.get_fps())}", "left")
+    game.debug(f"Position : {int(player.pos.x)} | {int(player.pos.y)}", "right")
+    game.debug(
+        f"Camera : {int(screen.camera.position.x)} | {int(screen.camera.position.y)} - {screen.camera.focused_object.__class__.__name__ if screen.camera.focused_object else "Free"}",
+        "left")
+
+    game.debug("Jump : " + ("True" if player.jump else "False"), "right")
+    game.debug("Fall : " + ("True" if player.fall else "False"), "right")
+    game.debug(f"Velocity : {player.vel.x:.1f} | {player.vel.y:.1f}" , "right")
+
+    if player.collided_objs:
+        game.debug("Collisions :", "right")
+        for collision in player.collided_objs:
+            game.debug(f"{collision[0].__class__.__name__} | {collision[1]}", "right", 22)
+
+
 
 def loop():
+    game.screen.default_surface.fill((0,0,0,0))
 
     player.set_animation(anim)
 
-    screen = game.screen
+    
     screen.set_layer(1, "#object")
-    screen.set_layer(2, "player")
+    screen.set_layer(2, "#player")
 
-    screen.add(block, "#object")
+    debug_info()
 
-    screen.add(player, "player")
+    for colls in collections:
+        screen.add(colls, "#object")
+
+    screen.add(player, "#player")
     screen.set_background(p_bg)
     screen.camera.focus(player)
-
-    keys_pressed = pg.key.get_pressed()
-
-
-
-    if keys_pressed[pg.K_d]:
-       player.set_position(player.pos + (1.5, 0))
-       player.set_direction("right")
-    if keys_pressed[pg.K_q]:
-       player.set_position(player.pos + (-1.5, 0))
-       player.set_direction("left")
-    if keys_pressed[pg.K_s]:
-        player.set_position(player.pos + (0, 1.5))
-    if keys_pressed[pg.K_z]:
-        player.set_position(player.pos + (0, -1.5))
 
 def main():
     game.run(loop)
