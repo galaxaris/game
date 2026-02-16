ddb157a999e2da2b184b755c3b77c08b5e88d0b6
Raphix <picot.raphael@gmail.com>
2026-02-16 11:34:13 +0100

    feat : add debug tools

--- main.py
+++ main.py
@@ -28,7 +28,13 @@ game.set_icon(join("assets", "icon.jpg"))
 game.enable_debug()
 
 glob = Resource(ResourceType.GLOBAL, "assets")
-anim = Animation(Texture("run.png", glob), 12, 100)
+
+run_anim = Animation(Texture("player/run.png", glob), 12, 100)
+run_fast_anim = Animation(Texture("player/run.png", glob), 12, 70)
+idle_anim = Animation(Texture("player/idle.png", glob), 11, 100)
+jump_anim = Texture("player/jump.png", glob)
+fall_anim = Texture("player/fall.png", glob)
+
 
 blue_tile = Texture("Blue.png", glob)
 
@@ -41,14 +47,25 @@ player = Player((-10, 0), (50, 50))
 player.set_gravity(0.5)
 player.set_position((310,410))
 
+player.bind_animations({
+    "run": run_anim,
+    "run_fast": run_fast_anim,
+    "idle": idle_anim,
+    "jump": jump_anim,
+    "fall": fall_anim,
+
+})
+
 collections = [Solid((x,600), (100, 100)) for x in range(0, 2000, 100)]
 collections += [Solid((0,y), (100, 100)) for y in range(200, 700, 100)]
+collections += [Solid((250, 550), (200, 20))]
+collections += [Solid((550, 500), (200, 50))]
 
 for coll in collections:
     coll.set_color((200, 200, 200))
 
 game.screen.camera.set_offset((RENDER_WIDTH//2 - player.size.x,RENDER_HEIGHT//2 - player.size.y))
-game.screen.camera.set_limits((-RENDER_WIDTH*3, -RENDER_HEIGHT-100), (RENDER_WIDTH*3, RENDER_HEIGHT-100))
+game.screen.camera.set_limits((47, -RENDER_HEIGHT-100), (RENDER_WIDTH*3, RENDER_HEIGHT-100))
 
 p_bg = ParallaxBackground((RENDER_WIDTH, RENDER_HEIGHT), [
     ParallaxLayer(pg.Vector2(0.9, 0.45), t_p1),
@@ -61,28 +78,11 @@ screen = game.screen
 
 
 def debug_info():
-    game.debug(f"FPS : {int(game.clock.get_fps())}", "left")
-    game.debug(f"Position : {int(player.pos.x)} | {int(player.pos.y)}", "right")
-    game.debug(
-        f"Camera : {int(screen.camera.position.x)} | {int(screen.camera.position.y)} - {screen.camera.focused_object.__class__.__name__ if screen.camera.focused_object else "Free"}",
-        "left")
-
-    game.debug("Jump : " + ("True" if player.jump else "False"), "right")
-    game.debug("Fall : " + ("True" if player.fall else "False"), "right")
-    game.debug("Boost : " + ("True" if player.boost else "False"), "right")
-    game.debug(f"Velocity : {player.vel.x:.1f} | {player.vel.y:.1f}" , "right")
-
-    if player.collided_objs:
-        game.debug("Collisions :", "right")
-        for collision in player.collided_objs:
-            game.debug(f"{collision[0].__class__.__name__} | {collision[1]}", "right", 22)
-
+    game.register_debug_entity(player)
 
 
 def loop():
     game.screen.default_surface.fill((0,0,0,0))
-
-    player.set_animation(anim)
 
     screen.set_layer(1, "#object")
     screen.set_layer(2, "#player")
@@ -95,6 +95,9 @@ def loop():
     screen.add(player, "#player")
     screen.set_background(p_bg)
     screen.camera.focus(player)
+
+
+
 
 def main():
     game.run(loop)
