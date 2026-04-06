74c0311e46aa098eda82a608c4efd34d40508089
GauGoth-Corp <ggaugoth@gmail.com>
2026-04-06 10:52:04 +0200

    Feat: adding more organization

---
+++ setup/event_manager.py
@@ -0,0 +1,7 @@
+"""
+This module allows you manage and add events to the EventManager. 
+
+Based on `api.events.EventManager` and `api.events.DefaultEventCollection`
+
+Grab your new events from the scripts you have defined in `scripts/` (particularly `scripts/event.py`)
+"""
---
+++ setup/scene_manager.py
@@ -0,0 +1,3 @@
+"""
+This module manages the registering, loading & switching of game scenes (menus, levels...). Based on `api.SceneManager` module
+"""
