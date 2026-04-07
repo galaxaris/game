ddb0405af6e9826fcc8a69fb261114e60ed7db83
Aomm-lgtm <lisalechani@gmail.com>
2026-04-07 10:28:46 +0200

    feat: added anchor block

--- setup/game.py
+++ setup/game.py
@@ -81,6 +81,10 @@ class Omicronde:
         self.collections += [Solid((250, 550), (200, 20))] # First platform
         self.collections += [Solid((550, 500), (500, 50))] # Second platform
 
+        anchor_point = Solid((600, 300), (20, 20))
+        anchor_point.add_tag("anchor")
+        self.collections += [anchor_point]
+
         for coll in self.collections:
             coll.set_color((200, 200, 200))
             coll.set_texture(self.RESSOURCES["textures"]["grass"])
