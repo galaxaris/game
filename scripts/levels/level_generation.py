9e44057218c3525725178f41c53f885c7b97fa07
Raphix <picot.raphael@gmail.com>
2026-04-12 09:11:14 +0200

    feat: add parallax for each level

--- scripts/levels/level_generation.py
+++ scripts/levels/level_generation.py
@@ -5,14 +5,11 @@ from api.environment.Trigger import TriggerKillBox
 from game.scripts.player_manager import init_player
 import pygame as pg
 
+
 def init_level(game: Game, scene: Scene, player):
     game.event_manager.Instances.bindInstance("player", player)
     scene.add(player, "#player")
-    p_bg = ParallaxBackground(game.render_size, [
-        ParallaxLayer(pg.Vector2(0.9, 0.45), game.RESSOURCES["textures"]["parallax1"]),
-        ParallaxLayer(pg.Vector2(0.7, 0.35), game.RESSOURCES["textures"]["parallax2"]),
-        ParallaxLayer(pg.Vector2(0.5, 0.25), game.RESSOURCES["textures"]["parallax3"]),
-    ], (75, 105, 52))
+    p_bg = get_parallax_background(scene.name, game)
     scene.set_background(p_bg)
     scene.camera.set_offset((scene.size.x // 2 - player.size.x, scene.size.y // 2 - player.size.y))
     scene.camera.set_limits((100, -scene.size.y - 100), (scene.size.x * 20, scene.size.y - 100))
@@ -22,3 +19,29 @@ def init_level(game: Game, scene: Scene, player):
 def create_killBoxes(game, scene, player, length):
     trigger_box = TriggerKillBox((-1000, scene.size.y+400), (length*1000, 100), ["player", "projectile"], once=False, sfx=["death"])
     scene.add(trigger_box, "#objects")
+
+
+def get_parallax_background(name, game):
+    p_bg = None
+    if name == "Level1Scene" or name == "Level2Scene":
+        p_bg = ParallaxBackground(game.render_size, [
+            ParallaxLayer(pg.Vector2(0.9, 0.45), game.RESSOURCES["textures"]["forest_layer3"]),
+            ParallaxLayer(pg.Vector2(0.7, 0.35), game.RESSOURCES["textures"]["forest_layer2"]),
+            ParallaxLayer(pg.Vector2(0.5, 0.25), game.RESSOURCES["textures"]["forest_layer1"]),
+        ], (75, 105, 52))
+    if name == "Level3Scene":
+        p_bg = ParallaxBackground(game.render_size, [
+            ParallaxLayer(pg.Vector2(0.9, 0.45), game.RESSOURCES["textures"]["desert_layer5"]),
+            ParallaxLayer(pg.Vector2(0.8, 0.40), game.RESSOURCES["textures"]["desert_layer4"]),
+            ParallaxLayer(pg.Vector2(0.7, 0.35), game.RESSOURCES["textures"]["desert_layer3"]),
+            ParallaxLayer(pg.Vector2(0.5, 0.30), game.RESSOURCES["textures"]["desert_layer2"]),
+            ParallaxLayer(pg.Vector2(0.3, 0.25), game.RESSOURCES["textures"]["desert_layer1"]),
+        ], (75, 105, 52))
+    if name == "BossLevelScene":
+        p_bg = ParallaxBackground(game.render_size, [
+            ParallaxLayer(pg.Vector2(0.9, 0.45), game.RESSOURCES["textures"]["industrial_layer1"]),
+            ParallaxLayer(pg.Vector2(0.7, 0.35), game.RESSOURCES["textures"]["industrial_layer2"]),
+            ParallaxLayer(pg.Vector2(0.5, 0.25), game.RESSOURCES["textures"]["industrial_layer3"]),
+            ParallaxLayer(pg.Vector2(0.3, 0.15), game.RESSOURCES["textures"]["industrial_bg"]),
+        ], (75, 105, 52))
+    return p_bg
