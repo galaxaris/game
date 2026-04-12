df29b7b6684e9ff11262a55012aed78fa449471e
Raphix <picot.raphael@gmail.com>
2026-04-12 17:12:42 +0200

    Add Base and First part of Story

--- root.py
+++ root.py
@@ -40,7 +40,7 @@ scene = game.scene # Alias
 from game.scripts.run import update
 from game.scripts.events import *
 register_events(game)
-load_scene("Level3Scene", game)
+load_scene("MainMenuScene", game)
 game.run(lambda: update(game))
 
 
